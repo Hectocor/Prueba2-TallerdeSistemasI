7efa6e99c847ebf331a2c741adfdc33b9373fbf4
Hector Orellana <hor_93@hotmail.com>
2017-06-02 12:43:27 -0400

    VS-0.3

--- src/java/cl/contexto/contexto.java
+++ src/java/cl/contexto/contexto.java
@@ -17,9 +17,15 @@ public class contexto implements ServletContextListener{
     public void contextInitialized(ServletContextEvent sce) {
     
         List<Vehiculo> listaVehiculos;
-        listaVehiculos = new ArrayList();
-        
-        
+        listaVehiculos = new ArrayList<>();
+        // Se añade a la lista el vehiculo segun parametros Patente, Marca, Modelo, Color, Año fabricación, Estado
+        listaVehiculos.add(new Vehiculo("AS-ZX-12", "Toyota", "Yaris", "Rojo", 2010, "DISPONIBLE"));
+        listaVehiculos.add(new Vehiculo("DF-ER-11", "Chevrolet", "Luv", "Blanco", 1990, "DISPONIBLE"));
+        listaVehiculos.add(new Vehiculo("HY-CC-42", "Ford", "Escape", "Azul", 2009, "DISPONIBLE"));
+        listaVehiculos.add(new Vehiculo("MN-CT-59", "Renault", "Clio", "Blanco", 2013, "DISPONIBLE"));
+        listaVehiculos.add(new Vehiculo("UI-UI-09", "Nissan", "V16", "Verde", 2000, "DISPONIBLE"));
+        listaVehiculos.add(new Vehiculo("RT-IL-89", "Volkswagen", "Saveiro", "Negro", 2017, "DISPONIBLE"));
+                                         
     }
 
     @Override
--- src/java/cl/modelo/Usuario.java
+++ src/java/cl/modelo/Usuario.java
@@ -9,9 +9,34 @@ public class Usuario {
     public Usuario(){
 
     }
+    
     public Usuario(String login,String password,String rol){
         this.login=login;
         this.password=password;
         this.rol=rol;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
     }
 }
